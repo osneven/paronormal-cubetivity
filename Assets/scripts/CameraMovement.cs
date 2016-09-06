1bb1a1935d134e4c3e566b356e0edbf11987fe53
osneven <oliver@neven.dk>
2016-09-06 10:22:15 +0200

    Initial commit

---
+++ Assets/scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+    private GameObject player;
+    private GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+        camera = GameObject.Find("Main Camera");
+        player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Make sure that the Main Camera follows the players x position
+        camera.transform.position = new Vector3(player.transform.position.x, 5, -10);
+	}
+}
---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private GameObject player;
+    private Rigidbody playerBody;
+    private Vector3 jumpVelocity;
+    private Vector3 passiveVelocity;
+
+    // Use this for initialization
+    void Start() {
+        // Initialize player fields
+        player = GameObject.Find("Player");
+        playerBody = this.GetComponent<Rigidbody>();
+
+        // Initialize velocity fields
+        jumpVelocity = new Vector3(0, 7, 0);
+        passiveVelocity = new Vector3(.1f, 0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update() {
+
+        // Controll jump
+        if (Input.GetKeyDown("space") && CanJump()) playerBody.velocity += jumpVelocity;
+
+        // Controll passive movement
+        player.transform.position += passiveVelocity;
+	}
+
+    // Checks if the player is able to jump at the current moment
+    bool CanJump() {
+        return playerBody.velocity.y == 0;
+    }
+}
